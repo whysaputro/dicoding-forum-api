8f6de32f4121d79fd5e42f8694f866ed25f686e8
Hendra Wahyu <hendrawahyu26@gmail.com>
2022-02-11 10:06:06 +0700

    create ThreadRepository interface

---
+++ src/Domains/threads/ThreadRepository.js
@@ -0,0 +1,15 @@
+class ThreadRepository {
+  async addThread(newThread) {
+    throw new Error('THREAD_REPOSITORY.METHOD_NOT_IMPLEMENTED');
+  }
+
+  async getThreadById(threadId) {
+    throw new Error('THREAD_REPOSITORY.METHOD_NOT_IMPLEMENTED');
+  }
+
+  async verifyThreadIsExistById(threadId) {
+    throw new Error('THREAD_REPOSITORY.METHOD_NOT_IMPLEMENTED');
+  }
+}
+
+module.exports = ThreadRepository;
---
+++ src/Domains/threads/_test/ThreadRepository.test.js
@@ -0,0 +1,16 @@
+const ThreadRepository = require('../ThreadRepository');
+
+describe('ThreadRepository interface', () => {
+  it('should throw error when invoke abstract behaviour', () => {
+    // Arrange
+    const threadRepository = new ThreadRepository();
+
+    // Action and assert
+    expect(() => threadRepository.addThread({}))
+      .rejects.toThrowError('THREAD_REPOSITORY.METHOD_NOT_IMPLEMENTED');
+    expect(() => threadRepository.getThreadById('')
+      .rejects.toThrowError('THREAD_REPOSITORY.METHOD_NOT_IMPLEMENTED'));
+    expect(() => threadRepository.verifyThreadIsExistById('')
+      .rejects.toThrowError('THREAD_REPOSITORY.METHOD_NOT_IMPLEMENTED'));
+  });
+});
