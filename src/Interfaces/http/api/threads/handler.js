49c03db652db9504573022c90b95c12860d46970
Whysaputro <hendrawahyu26@gmail.com>
2022-02-12 16:48:14 +0700

    add /threads/{threadId}/comments endpoint

--- src/Interfaces/http/api/threads/handler.js
+++ src/Interfaces/http/api/threads/handler.js
@@ -1,10 +1,12 @@
 const AddNewThreadUseCase = require('../../../../Applications/use_case/AddNewThreadUseCase');
+const AddNewCommentUseCase = require('../../../../Applications/use_case/AddNewCommentUseCase');
 
 class ThreadsHandler {
   constructor(container) {
     this._container = container;
 
     this.postAddNewThreadHandler = this.postAddNewThreadHandler.bind(this);
+    this.postAddNewCommentHandler = this.postAddNewCommentHandler.bind(this);
   }
 
   async postAddNewThreadHandler(request, h) {
@@ -16,6 +18,23 @@ class ThreadsHandler {
       status: 'success',
       data: {
         addedThread,
+      },
+    });
+
+    response.code(201);
+    return response;
+  }
+
+  async postAddNewCommentHandler(request, h) {
+    const { threadId } = request.params;
+    const addNewCommentUseCase = this._container.getInstance(AddNewCommentUseCase.name);
+    const addedComment = await addNewCommentUseCase
+      .execute(request.payload, threadId, request.auth.credentials);
+
+    const response = h.response({
+      status: 'success',
+      data: {
+        addedComment,
       },
     });
 
