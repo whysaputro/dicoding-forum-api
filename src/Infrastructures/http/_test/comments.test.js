0ef52e13a99529e7ed9e60c7463e7e5a3111ba44
Whysaputro <hendrawahyu26@gmail.com>
2022-02-12 17:26:15 +0700

    create functional test for add comment

--- src/Infrastructures/http/_test/comments.test.js
+++ src/Infrastructures/http/_test/comments.test.js
@@ -53,14 +53,12 @@ describe('/threads/{threadId}/comments endpoint', () => {
       // Arrange
       const server = await createServer(container);
       const threadId = 'thread-123';
-      console.log(await AuthenticationTestHelper.getAccessTokenHelper(server));
-      const requestPayload = {
-        content: 'sebuah comment',
-      };
+      const accessToken = await AuthenticationTestHelper.getAccessTokenHelper(server);
+      const requestPayload = { };
 
-      // await UsersTableTestHelper.addUser({
-      //   id: 'user-123',
-      // });
+      await UsersTableTestHelper.addUser({
+        id: 'user-123',
+      });
 
       await ThreadsTableTestHelper.addNewThread({
         id: threadId,
@@ -82,7 +80,81 @@ describe('/threads/{threadId}/comments endpoint', () => {
       expect(response.statusCode).toEqual(400);
       expect(responseJson.status).toEqual('fail');
       expect(responseJson.message)
-        .toEqual('tidak dapat membuat thread baru karena properti yang dibutuhkan tidak ada');
+        .toEqual('tidak dapat membuat comment baru karena properti yang dibutuhkan tidak ada');
+    });
+
+    it('should response with 400 when payload not meet data specifications', async () => {
+      // Arrange
+      const server = await createServer(container);
+      const threadId = 'thread-123';
+      const accessToken = await AuthenticationTestHelper.getAccessTokenHelper(server);
+      const requestPayload = {
+        content: true,
+      };
+
+      await UsersTableTestHelper.addUser({
+        id: 'user-123',
+      });
+
+      await ThreadsTableTestHelper.addNewThread({
+        id: threadId,
+        owner: 'user-123',
+      });
+
+      // Action
+      const response = await server.inject({
+        method: 'POST',
+        url: `/threads/${threadId}/comments`,
+        payload: requestPayload,
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+        },
+      });
+
+      // Assert
+      const responseJson = JSON.parse(response.payload);
+      expect(response.statusCode).toEqual(400);
+      expect(responseJson.status).toEqual('fail');
+      expect(responseJson.message)
+        .toEqual('tidak dapat membuat comment baru karena tipe data tidak sesuai');
+    });
+
+    it('should response with 201 and persisted comment', async () => {
+      // Arrange
+      const server = await createServer(container);
+      const threadId = 'thread-123';
+      const accessToken = await AuthenticationTestHelper.getAccessTokenHelper(server);
+      const requestPayload = {
+        content: 'sebuah comment',
+      };
+
+      await UsersTableTestHelper.addUser({
+        id: 'user-123',
+      });
+
+      await ThreadsTableTestHelper.addNewThread({
+        id: threadId,
+        owner: 'user-123',
+      });
+
+      // Action
+      const response = await server.inject({
+        method: 'POST',
+        url: `/threads/${threadId}/comments`,
+        payload: requestPayload,
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+        },
+      });
+
+      // Assert
+      const responseJson = JSON.parse(response.payload);
+      expect(response.statusCode).toEqual(201);
+      expect(responseJson.status).toEqual('success');
+      expect(responseJson.data.addedComment).toBeDefined();
+      expect(responseJson.data.addedComment.id).toBeDefined();
+      expect(responseJson.data.addedComment.content).toBeDefined();
+      expect(responseJson.data.addedComment.owner).toBeDefined();
     });
   });
 });
