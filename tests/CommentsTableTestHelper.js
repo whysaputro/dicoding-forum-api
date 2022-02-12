94cb930c8365a6fe490b391c7e8b26945b8c2c7f
Whysaputro <hendrawahyu26@gmail.com>
2022-02-12 15:10:07 +0700

    add method findCommentById in CommentTableTestHelper

--- tests/CommentsTableTestHelper.js
+++ tests/CommentsTableTestHelper.js
@@ -2,6 +2,17 @@
 const pool = require('../src/Infrastructures/database/postgres/pool');
 
 const CommentsTableTestHelper = {
+  async findCommentById(commentId) {
+    const query = {
+      text: 'SELECT * FROM comments WHERE id = $1',
+      values: [commentId],
+    };
+
+    const result = await pool.query(query);
+
+    return result.rows;
+  },
+
   async cleanTable() {
     await pool.query('DELETE FROM comments WHERE 1=1');
   },
