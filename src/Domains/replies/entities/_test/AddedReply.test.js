fffbc18ecbc9984fafccbd1a7343f3bcb6903e46
Whysaputro <hendrawahyu26@gmail.com>
2022-02-14 10:20:57 +0700

    create AddedReply entities

---
+++ src/Domains/replies/entities/_test/AddedReply.test.js
@@ -0,0 +1,43 @@
+const AddedReply = require('../AddedReply');
+
+describe('a AddedReply entities', () => {
+  it('should throw error when payload did not contain needed property', () => {
+    // Arrange
+    const payload = {
+      content: 'abc',
+      owner: 'user-123',
+    };
+
+    // Action & Assert
+    expect(() => new AddedReply(payload)).toThrowError('ADDED_REPLY.NOT_CONTAIN_NEEDED_PROPERTY');
+  });
+
+  it('should throw error when payload not meet data type specifications', () => {
+    // Arrange
+    const payload = {
+      id: true,
+      content: 'sebuah balasan',
+      owner: {},
+    };
+
+    // Action & Assert
+    expect(() => new AddedReply(payload)).toThrowError('ADDED_REPLY.NOT_MEET_DATA_TYPE_SPECIFICATION');
+  });
+
+  it('should create addedReply object correctly', () => {
+    // Arrange
+    const payload = {
+      id: 'reply-123',
+      content: 'sebuah balasan',
+      owner: 'user-123',
+    };
+
+    // Action
+    const { id, content, owner } = new AddedReply(payload);
+
+    // Assert
+    expect(id).toEqual(payload.id);
+    expect(content).toEqual(payload.content);
+    expect(owner).toEqual(payload.owner);
+  });
+});
